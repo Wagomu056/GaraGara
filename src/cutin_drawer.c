fe981a25bba3429c79b89effcda348c12b2ae620
Takuya Azumaguchi <catchballer@gmail.com>
2022-07-09 22:57:20 +0900

    Draw boarder by image

--- src/cutin_drawer.c
+++ src/cutin_drawer.c
@@ -5,11 +5,10 @@
 #include "cutin_drawer.h"
 #include "defines.h"
 #include "math_util.h"
+#include "pd_uitl.h"
 
-static const float DISPLAY_CENTER = 120.0f;
-static const int BOARDER_WIDTH = 2;
-
-static const int DOUBLE_LINE_GAP = 5;
+static const float DISPLAY_CENTER_X = DISPLAY_WIDTH * 0.5f;
+static const float DISPLAY_CENTER_Y = DISPLAY_HEIGHT * 0.5f;
 
 static const float MOVE_SPEED = 1.0f / 10.0f;
 static const float MOVE_START = 0.0;
@@ -19,15 +18,35 @@ static PlaydateAPI* _pd;
 static float _moveRatio = 1.0f;
 static int _isStarted = 0;
 
+static LCDBitmap *_image = NULL;
+
+static const int BOARDER_NUM = 2;
+static LCDSprite *_sprites[BOARDER_NUM] = {NULL};
+
 void initCutin(PlaydateAPI* pd)
 {
     _pd = pd;
+    _image = loadImageAtPath(pd, "images/boarder.png");
+
+    for (int i = 0; i < BOARDER_NUM; i++)
+    {
+        LCDSprite *sprite = _pd->sprite->newSprite();
+        _pd->sprite->setImage(sprite, _image, kBitmapUnflipped);
+        _pd->sprite->setZIndex(sprite, 999);
+        _sprites[i] = sprite;
+    }
 }
 
 void startCutin(void)
 {
     _moveRatio = 0.0f;
     _isStarted = 1;
+
+    for (int i = 0; i < BOARDER_NUM; i++)
+    {
+        _pd->sprite->addSprite(_sprites[i]);
+        _pd->sprite->moveTo(_sprites[i], DISPLAY_CENTER_X, DISPLAY_CENTER_Y);
+    }
 }
 
 static void updateMoveRatio(float *ratio)
@@ -40,17 +59,9 @@ static void updateMoveRatio(float *ratio)
     }
 }
 
-static void drawBorderLine(float y)
+static void moveBoarder(int idx, float y)
 {
-    const float dy = DISPLAY_CENTER + y;
-    _pd->graphics->drawLine(0, dy, DISPLAY_WIDTH, dy, BOARDER_WIDTH,
-                            kColorBlack);
-}
-
-static void drawDoubleLine(float y, float gap)
-{
-    drawBorderLine(y);
-    drawBorderLine(y - gap);
+    _pd->sprite->moveTo(_sprites[idx], DISPLAY_CENTER_X, DISPLAY_CENTER_Y + y);
 }
 
 void updateCutin(void)
@@ -61,14 +72,6 @@ void updateCutin(void)
 
     const float ratio = easeInOut(_moveRatio);
     const float lineY = linear(MOVE_START, MOVE_DEST, ratio);
-    drawDoubleLine(lineY, DOUBLE_LINE_GAP);
-    drawDoubleLine(-1.0f * lineY, -1.0f * DOUBLE_LINE_GAP);
-
-    _pd->graphics->fillRect( 0.0f, DISPLAY_CENTER + lineY,
-                             DISPLAY_WIDTH, -2.0f * lineY, kColorBlack );
-
-    const float CHAR_WIDTH = 45;
-    const float CHAR_HEIGHT = 45;
-    _pd->graphics->fillRect( 20.0f, DISPLAY_CENTER - (CHAR_HEIGHT * 0.5f),
-                             CHAR_WIDTH, CHAR_HEIGHT, kColorWhite );
+    moveBoarder(0, lineY);
+    moveBoarder(1, -1.0f * lineY);
 }

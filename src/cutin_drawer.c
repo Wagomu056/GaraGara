5bd963af134138ba65b8c14a7907c082c92fd2f8
Takuya Azumaguchi <catchballer@gmail.com>
2022-07-10 01:21:34 +0900

    Add background of cutin

--- src/cutin_drawer.c
+++ src/cutin_drawer.c
@@ -13,15 +13,18 @@ static const float DISPLAY_CENTER_Y = DISPLAY_HEIGHT * 0.5f;
 static const float MOVE_SPEED = 1.0f / 10.0f;
 static const float MOVE_START = 0.0;
 static const float MOVE_DEST = -75.0f;
+static const int SPRITE_Z = 900;
 
 static PlaydateAPI* _pd;
 static float _moveRatio = 1.0f;
 static int _isStarted = 0;
 
 static LCDBitmap *_image = NULL;
-
 static const int BOARDER_NUM = 2;
 static LCDSprite *_sprites[BOARDER_NUM] = {NULL};
+
+static LCDBitmap *_tileImage = NULL;
+static LCDSprite *_tileSprite = NULL;
 
 void initCutin(PlaydateAPI* pd)
 {
@@ -32,8 +35,20 @@ void initCutin(PlaydateAPI* pd)
     {
         LCDSprite *sprite = _pd->sprite->newSprite();
         _pd->sprite->setImage(sprite, _image, kBitmapUnflipped);
-        _pd->sprite->setZIndex(sprite, 999);
+        _pd->sprite->setZIndex(sprite, SPRITE_Z);
         _sprites[i] = sprite;
+    }
+
+    {
+        _tileImage = loadImageAtPath(pd, "images/back_wa.png");
+        _pd->graphics->tileBitmap(_tileImage, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, kBitmapUnflipped);
+
+        LCDSprite *sprite = _pd->sprite->newSprite();
+        _pd->sprite->setImage(sprite, _tileImage, kBitmapUnflipped);
+        _pd->sprite->setZIndex(sprite, SPRITE_Z);
+        _pd->sprite->moveTo(sprite, DISPLAY_CENTER_X, DISPLAY_CENTER_Y);
+
+        _tileSprite = sprite;
     }
 }
 
@@ -47,6 +62,7 @@ void startCutin(void)
         _pd->sprite->addSprite(_sprites[i]);
         _pd->sprite->moveTo(_sprites[i], DISPLAY_CENTER_X, DISPLAY_CENTER_Y);
     }
+    _pd->sprite->addSprite(_tileSprite);
 }
 
 static void updateMoveRatio(float *ratio)
@@ -74,4 +90,8 @@ void updateCutin(void)
     const float lineY = linear(MOVE_START, MOVE_DEST, ratio);
     moveBoarder(0, lineY);
     moveBoarder(1, -1.0f * lineY);
+
+    LCDRect rect = {0, DISPLAY_WIDTH,
+                    DISPLAY_CENTER_Y + lineY + 8, DISPLAY_CENTER_Y + -1.0f * lineY - 8};
+    _pd->sprite->setClipRect(_tileSprite, rect);
 }

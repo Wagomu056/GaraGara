f98e41ef43d2737e9462dc7b1613af8e0ef88265
Takuya Azumaguchi <catchballer@gmail.com>
2022-07-09 22:56:59 +0900

    Add pd_util

---
+++ src/pd_uitl.h
@@ -0,0 +1,12 @@
+//
+// Created by 東口拓也 on 2022/07/09.
+//
+
+#ifndef BOUND_BALL_PD_UITL_H
+#define BOUND_BALL_PD_UITL_H
+
+#include <pd_api.h>
+
+LCDBitmap *loadImageAtPath(PlaydateAPI* pd, const char *path);
+
+#endif //BOUND_BALL_PD_UITL_H
---
+++ src/pd_util.c
@@ -0,0 +1,16 @@
+//
+// Created by 東口拓也 on 2022/07/09.
+//
+
+#include "pd_uitl.h"
+
+LCDBitmap *loadImageAtPath(PlaydateAPI* pd, const char *path)
+{
+    const char *outErr = NULL;
+    LCDBitmap *img = pd->graphics->loadBitmap(path, &outErr);
+    if ( outErr != NULL ) {
+        pd->system->logToConsole("Error loading image at path '%s': %s", path, outErr);
+    }
+    return img;
+}
+

529778eca754856678675f2251b7768dd5d18472
Takuya Azumaguchi <catchballer@gmail.com>
2022-07-10 02:08:53 +0900

    Use pd_util in name_drawer

--- src/name_drawer.c
+++ src/name_drawer.c
@@ -2,21 +2,13 @@
 // Created by 東口拓也 on 2022/07/09.
 //
 #include "name_drawer.h"
+#include "log.h"
+#include "pd_uitl.h"
 
 static PlaydateAPI* _pd;
 
 static LCDBitmap *_ballImage = NULL;
 static LCDSprite *_sprites[8] = {NULL};
-
-static LCDBitmap *loadImageAtPath(PlaydateAPI* pd, const char *path)
-{
-    const char *outErr = NULL;
-    LCDBitmap *img = pd->graphics->loadBitmap(path, &outErr);
-    if ( outErr != NULL ) {
-        pd->system->logToConsole("Error loading image at path '%s': %s", path, outErr);
-    }
-    return img;
-}
 
 void initName(PlaydateAPI* pd)
 {
@@ -25,8 +17,6 @@ void initName(PlaydateAPI* pd)
     _ballImage = loadImageAtPath(pd, "images/33.png");
 
     LCDSprite *sprite = _pd->sprite->newSprite();
-    _pd->graphics->getBitmapData(_ballImage, NULL, NULL,
-                                 NULL, NULL, NULL);
     _pd->sprite->setImage(sprite, _ballImage, kBitmapUnflipped);
     _pd->sprite->setZIndex(sprite, 999);
 

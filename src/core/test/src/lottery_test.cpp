3db815d64e7bd46e288cb6d2bf08346fd318b9cf
Takuya Azumaguchi <catchballer@gmail.com>
2022-07-17 16:18:09 +0900

    Add registered check to lottery test

--- src/core/test/src/lottery_test.cpp
+++ src/core/test/src/lottery_test.cpp
@@ -14,18 +14,27 @@ TEST(Lottery, lotteryOne) {
 }
 
 static std::vector<std::string> returns;
+static std::vector<std::string> registered;
 TEST(Lottery, lotteryMulti) {
-    registerLottery("ABC");
-    registerLottery("EFG");
-    registerLottery("HIJ");
-    registerLottery("KLM");
+    registerLottery("ABC"); registered.push_back("ABC");
+    registerLottery("EFG"); registered.push_back("EFG");
+    registerLottery("HIJ"); registered.push_back("HIJ");
+    registerLottery("KLM"); registered.push_back("KLM");
 
     for (int i = 0; i < 4; i++) {
         const char *ret = lottery();
         for (auto pair : returns) {
             EXPECT_STRNE(pair.c_str(), ret);
         }
-
         returns.push_back(ret);
+
+        bool exists = false;
+        for (auto pair : registered) {
+            if (strcmp(pair.c_str(), ret) == 0) {
+                exists = true;
+                break;
+            }
+        }
+        EXPECT_TRUE(exists);
     }
 }
